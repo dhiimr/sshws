1c005ddacfac3e3e8c90c3a5efb5585acbcbac2e
dhiimr <51095316+dhiimr@users.noreply.github.com>
2021-11-11 19:14:56 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-mkdir -p /root/.mrz/kerja
+#!/bin/bash
+mrz=/root/.mrz
+mkdir -p $mrz
+cd $mrz
 read -n "Domain Name : "
 read domain
 sudo apt install stunnel dropbear gcc make cmake build-essential python unzip zip -y
@@ -7,5 +10,23 @@ sudo systemctl start apache2
 sudo certbot --apache -e dhiimr93@gmail.com -d $domain
 cp /etc/letsencrypt/live/$domain/*.pem /etc/stunnel/
 cat /etc/stunnel/{fullchain.pem,privkey.pem} >> /etc/stunnel/cmrz.pem
+wget https://github.com/dhiimr/sshws/raw/main/master.zip
+unzip master.zip
+cd badvpn-master/
+mkdir -p build
+cd build
+cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1
+make install
+cd $mrz
+systemctl stop apache2
+apt purge apache2
+wget https://raw.githubusercontent.com/dhiimr/sshws/main/PDirect.py
+wget https://raw.githubusercontent.com/dhiimr/sshws/main/svpn.service
+mv svpn.service /etc/systemd/system/
+wget https://raw.githubusercontent.com/dhiimr/sshws/main/wssh.service
+mv wssh.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl start wssh
+systemctl start svpn
 
 
